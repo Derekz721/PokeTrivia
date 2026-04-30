895e5f5c31311832538a3dbe68aab05c34bad92d
Derekz721 <derek.zheng22@bcmail.cuny.edu>
2026-04-30 17:30:21 -0400

    Create supabase.js

---
+++ client/src/supabase/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+    "https://supabase.com/dashboard/project/dcwoanjbzrztswibkfmp",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjd29hbmpienJ6dHN3aWJrZm1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0MDUwNTQsImV4cCI6MjA5Mjk4MTA1NH0.o4fXVxePUOxXSv_StqTMvMxOkc5qtjv82qvnE7sw-ik"
+);
