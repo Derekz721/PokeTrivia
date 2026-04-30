c59b26ecefaf51a5d186425747a12f1474dc75fa
Derekz721 <derek.zheng22@bcmail.cuny.edu>
2026-04-30 17:32:39 -0400

    Update server.js
    Finish server

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+// Serve React build in production
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+app.listen(3000, () => {
+  console.log("PokeTrivia server running on port 3000");
+});
